ac60aef099eae6b8d854eae83970ef959bb81e22
Mihnea Bejinaru <mihneabejinaru@yahoo.com>
2023-10-31 02:54:02 +0200

    add env variables

--- services/doctori/main.go
+++ services/doctori/main.go
@@ -26,27 +26,43 @@ func main() {
 		// }()
 	*/
 
+	// setup logger
 	log.SetOutput(os.Stdout) // Set log output to the stdout
 	log.Println("Application starting...")
 
-	config, err := config.LoadConfig(utils.CONFIG_PATH)
+	// load config file
+	conf, err := config.LoadConfig(utils.CONFIG_PATH)
 	if err != nil {
 		log.Fatalf("Error loading the config file: %s", err)
 	} else {
 		log.Println("Successfully loaded the config file.")
 	}
 
-	app, err := application.New(config)
+	/*
+		// // load .env vars (ONLY WHEN TESTING LOCALLY)
+		// err = godotenv.Load()
+		// if err != nil {
+		// 	log.Fatal("Error loading .env file")
+		// }
+	*/
+
+	// load .env vars into the config
+	config.ReplacePlaceholdersInStruct(conf)
+
+	// init the app
+	app, err := application.New(conf)
 	if err != nil {
 		log.Fatalf("Error creating and initializing the application: %s", err)
 	} else {
 		log.Println("Application successfully initialized.")
 	}
 
+	// catch interrupt signal
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 	log.Println("OS interrupt signals captured. Application will gracefully shut down on interruption...")
 
+	// start the app with the created context
 	err = app.Start(ctx)
 	if err != nil {
 		log.Fatalf("Error starting the app: %v", err)
