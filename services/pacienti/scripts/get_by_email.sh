1320cf3064df9d32d1676448bcd1386c454fe04b
Mihnea Bejinaru <mihneabejinaru@yahoo.com>
2023-11-03 12:55:20 +0200

    test all routes for pacient

---
+++ services/pacienti/scripts/get_by_email.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Extract port from config.yaml
+PORT=$(yq e '.server.port' configs/config.yaml)
+
+EMAIL="ion.popescu@example.com"
+
+# Use the extracted port in curl or other commands
+curl -X GET "http://localhost:$PORT/pacienti/email/$EMAIL"
